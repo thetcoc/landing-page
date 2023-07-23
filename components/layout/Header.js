daf50e087dbfa597aee2516e6f22592e3b6801ba
@FamousPaul <famous@tech.rebasive.com>
2023-07-23 22:53:55 +0100

    feat: Add about us page

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
 import NavigationBar from './NavigationBar'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Header = () => {
+  const router = useRouter()
   return (
     <div className="grid z-10 content-center items-center grid-flow-col p-8 max-w-screen-xl mx-auto justify-between">
         <div className="relative h-[39px] w-[130px]">
-            <Image 
-                src="/images/logo.png"
-                layout='fill'
-                alt='logo'
-            />
+            <Link href="/">
+              <Image 
+                  src={router.pathname != "/" ? "/images/logo2.svg" : "/images/logo.png"}
+                  layout='fill'
+                  alt='logo'
+              />
+            </Link>
         </div>
-        <NavigationBar />
+        <NavigationBar 
+          navBarStyle={router.pathname !== "/" && "text-black"}
+        />
     </div>
   )
 }
